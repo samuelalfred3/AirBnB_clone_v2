0e8deff2e4c56a0fbd4e5652cb5d4a305899d162
samuelalfred3 <samuelalfrednjuguna@gmail.com>
2024-04-18 16:32:19 +0300

    Initial Commit

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+Routes:
+    - /: Display "Hello HBNB!"
+    - /hbnb: Display "HBNB"
+    - /c/<text>: Display "C " followed by the value of the text variable
+"""
+
+from flask import Flask
+from werkzeug.utils import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays "Hello HBNB!"
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def display_hbnb():
+    """
+    Displays "HBNB"
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def display_c_text(text):
+    """
+    Displays "C " followed by the value of the text variable
+    """
+    return "C {}".format(escape(text.replace("_", " ")))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+
---
+++ web_flask/README.md
@@ -0,0 +1,20 @@
+# 0x04. AirBnB clone - Web framework
+
+# Resources
+* Read or watch:
+
+What is a Web Framework?
+A Minimal Application
+Routing (except “HTTP Methods”)
+Rendering Templates
+Synopsis
+Variables
+Comments
+Whitespace Control
+List of Control Structures (read up to “Call”)
+Flask
+Jinja
+
+# More Info
+Install Flask
+$ pip3 install Flask
